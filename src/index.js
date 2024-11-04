f85f70f16891ee1764db53b0b64fafa34e22650e
AdnanAjredini1 <adnanajredini144@gmail.com>
2024-11-04 22:23:40 +0100

    render

--- src/index.js
+++ src/index.js
@@ -54,6 +54,7 @@ server.use(
     resave: false,
     saveUninitialized: true,
     cookie: {
+      secure: process.env.NODE_ENV === "production",
       httpOnly: true,
       sameSite: 'None',
       maxAge: 1000 * 60 * 60 * 60 * 244,
