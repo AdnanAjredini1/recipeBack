1e033be6e2a5ebb2585497656e3d9fc462c65a12
AdnanAjredini1 <adnanajredini144@gmail.com>
2024-11-05 19:19:06 +0100

    render

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ server.use(
     cookie: {
       maxAge: 1000 * 60 * 60 * 60 * 24,
       httpOnly: true,
-      secure: true,
+     secure: process.env.NODE_ENV === 'production',
       sameSite: 'None',
       domain: 'recipeback-ijkr.onrender.com',
     },
