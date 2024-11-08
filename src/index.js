4c1020160f9d6627150b3d2f40ef29198c79fcd3
AdnanAjredini1 <adnanajredini144@gmail.com>
2024-11-08 14:21:36 +0100

    render

--- src/index.js
+++ src/index.js
@@ -21,13 +21,14 @@ const io = new Server();
 
 export { io };
 
+const store = new session.MemoryStore();
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(
   session({
     store: new PgSession({
-      conString: process.env.DATABASE_HOST,
+      conString: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false },
     }),
     secret: process.env.SESSION_SECRET,
