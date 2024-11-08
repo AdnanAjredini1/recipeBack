beaa2912cb7bcdc7eb2f08432f0f200f7ce8b366
AdnanAjredini1 <adnanajredini144@gmail.com>
2024-11-08 14:22:38 +0100

    render

--- src/index.js
+++ src/index.js
@@ -27,18 +27,18 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(
   session({
-    store: new PgSession({
-      conString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
-    }),
+    // store: new PgSession({
+    //   conString: process.env.DATABASE_URL,
+    //   ssl: { rejectUnauthorized: false },
+    // }),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
 
     cookie: {
-    //  httpOnly: true,
-    //   secure: process.env.NODE_ENV === 'production',
-    //   sameSite: 'none',
+     httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'none',
       maxAge: 1000 * 60 * 60 * 60 * 24,
     },
   })
