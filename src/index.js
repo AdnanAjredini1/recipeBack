24508193e00caadfb8c9af7cc967c2c9d6be68f4
AdnanAjredini1 <adnanajredini144@gmail.com>
2024-11-10 13:21:43 +0100

    renderr

--- src/index.js
+++ src/index.js
@@ -52,13 +52,14 @@ server.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false, // Better for production
+    saveUninitialized: true,
+    name:"GOKU",
     cookie: {
-      maxAge: 1000 * 60 * 60 * 24, // Set the appropriate maxAge
-      secure: isProduction, // true only in production for HTTPS
-      httpOnly: true,
-      sameSite: isProduction ? "none" : "lax", // 'none' for cross-site cookies in production
-      domain: isProduction ? ".onrender.com" : undefined, // Apply domain only in production
+      maxAge: 1000 * 60 * 60 * 60 * 244,
+      secure: true, // Ensures cookies are only sent over HTTPS
+      httpOnly: true, // Prevents JavaScript access to cookies
+      sameSite: 'none',
+    
     },
   })
 );
